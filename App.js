998380006d98b607e0e0a5f548825ac260b8224d
GMoulidharan <moulidharansrv@gmail.com>
2024-11-05 11:14:03 +0530

    Episode -4 Part-1

--- App.js
+++ App.js
@@ -1,59 +1,91 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// JSX - is not HTML inside JS. Its HTML like or XML like syntax.
 
-//JSX (transpiled b4 it reaches the JS engine ) -> Parcel ->
-
-//JSX =>Bable transplie it to React.createElement => ReactElement - JS Object => HTMLElement(render).
-
-//React Element
 /*
-const heading = (
-    <h1 className="head" tabIndex="1">
-        Namaste React using JSX in React element
-    </h1>
-);
-*/
+* -Header
+    -Logo
+    -Nav Items
+* -Body
+    -Search
+    -Restaurant container
+        -restaurant card
+            -image
+            -Name of res,start,cuisine,delivery time
+* -Footer
+    -copy right
+    -links
+    -address
+    -contact
+
+*/ 
+
+const Header = () =>{
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className='logo' src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=restaurant-food&sf=&txt_keyword=All'></img>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = (props) =>{
+    const{resName, cuisine} = props
+    
+
+    return(
+        <div className='res-card'>
+            <img
+             className='res-logo'
+             alt='res-logo'
+             src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/njqtgcdmse85dblw0liq'></img>
+            <h3>{resName}</h3>
+            <h4>{cuisine}</h4>
+            <h4>4.4 Stars</h4>
+            <h4>38 minutes</h4>
+        </div>
+    );
+};
 
 
-const Title = () => (
-    <h1 className="head" tabIndex="5">
-        Namaste React using functional component
-    </h1>
-);
 
-
-const elem = <span>React Element</span>
-const title = (
-    <h1 className="head" tabIndex="5">
-        {elem}
-        Namaste React using JSX
-    </h1>
-);
-
-//React Functional Components
-
-// const HeadingComponent = () =>{
-//     return <h1 className="heading">Namaste React Functional Component</h1>
-// }
-
-//component composition
-
-const number = 1000;
-const HeadingComponent2 = () => (
-    <div id="container">    
-    {/* react element inside component */}    
-        {title}
-        {Title()}
-        <Title />
-        <Title></Title>
-        <h1 className="heading">Namaste React Functional Component</h1>
+const Body = () =>{
+    return(<div className='body'>
+        <div className='search'>
+            Search
+        </div>
+        <div className='restaurant-container'>
+        <RestaurantCard 
+            resName="Meghna Foods" 
+            cuisine="Briyani, North Indian, Asian"
+            
+        />
+        <RestaurantCard 
+            resName="KFC" 
+            cuisine="Burger, Fast Food"
+        />            
+        </div>
     </div>
-);
-
-
+    )
+}
+const AppLayout = () =>{
+    return (
+        <div className="App">
+            <Header />
+            <Body />
+        </div>
+    )
+}
 
 const root  = ReactDOM.createRoot(document.getElementById('root'));//displaying on to browser use ReactDOM
 
-root.render(<HeadingComponent2 />);
+root.render(<AppLayout />);
