ce642334bb772a3a2e84615bc6fd0558ea4b9a33
GMoulidharan <moulidharansrv@gmail.com>
2024-11-14 11:34:37 +0530

    Episode-7 RouterDom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from "./components/Header";
 import Body from './components/Body';
-
-
-
-
-
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 //not using keys(not acceptable) <<<<< index as keys <<<<< unique id(best practice)
 
 
@@ -19,6 +18,25 @@ const AppLayout = () =>{
     )
 }
 
+const appRouter = createBrowserRouter([
+    
+    {
+        path:"/",
+        element:<AppLayout />,
+        errorElement:<Error />
+    },
+    {
+        path:"/about",
+        element:<About />,
+        errorElement:<Error />
+    }, 
+    {
+        path:"/contact",
+        element:<Contact />,
+        errorElement:<Error />
+    },
+]);
+
 const root  = ReactDOM.createRoot(document.getElementById('root'));//displaying on to browser use ReactDOM
 
-root.render(<AppLayout />);
+root.render(<RouterProvider router={appRouter}/>);
